803c263b584e5e6b46d8ef03931cb0861996b906
Ujjwal Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-01-22 16:17:26 +0530

    Time: 9 ms (73.73%), Space: 11.4 MB (59.85%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,7 +1,4 @@
-class Solution {
-    static bool comparator(string a,string b){
-        return (a+b)>(b+a);
-    }
+class Solution{
 public:
     string largestNumber(vector<int>& nums) {
         int len=nums.size();
@@ -9,7 +6,7 @@ public:
         string res="";
         for(int i=0;i<len;i++)
             str[i]=to_string(nums[i]);
-        sort(str.begin(),str.end(),comparator);
+        sort(str.begin(),str.end(),[](string a,string b){return a+b>b+a;});
         if(str[0]=="0")
             return "0";
         for(int i=0;i<len;i++)
