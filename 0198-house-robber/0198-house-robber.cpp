76ef9cca78b3a8f7866ae55c2fa07362057554e6
Ujjwal Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-01-23 18:10:50 +0530

    Time: 5 ms (24.97%), Space: 7.8 MB (28.30%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,17 +15,29 @@ class Solution {
     //         return dp[len];
     //     return max(solveRec(nums,dp,len-2)+nums[len],solveRec(nums,dp,len-1));
     // }
-    int solveTab(vector<int> &nums){
+    // int solveTab(vector<int> &nums){
+    //     int len=nums.size();
+    //     vector<int>dp(len+1,0);
+    //     dp[0]=0;
+    //     dp[1]=nums[0];
+    //     for(int i=1;i<len;i++)
+    //         dp[i+1]=max(dp[i-1]+nums[i],dp[i]);
+    //     return dp[len];
+    // }
+    int solveOptimise(vector<int>nums){
         int len=nums.size();
-        vector<int>dp(len+1,0);
-        dp[0]=0;
-        dp[1]=nums[0];
-        for(int i=1;i<len;i++)
-            dp[i+1]=max(dp[i-1]+nums[i],dp[i]);
-        return dp[len];
+        int prev2=0;
+        int prev1=nums[0];
+        int tmp;
+        for(int i=1;i<len;i++){
+            tmp=prev1;
+            prev1=max(prev2+nums[i],prev1);
+            prev2=tmp;
+        }
+        return prev1;
     }
 public:
     int rob(vector<int>& nums) {
-        return solveTab(nums);
+        return solveOptimise(nums);
     }
 };
