ca17d5e172301213133a675c1cdccb5eb8a66df6
Ujjwal Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-01-31 00:44:40 +0530

    Time: 254 ms (50.68%), Space: 123.8 MB (95.50%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
+    // vector<int> rearrangeArray(vector<int>& nums) {
+    //     vector<int> positive;
+    //     vector<int> negative;
+    //     int len=nums.size();
+    //     for(int i=0;i<len;i++){
+    //         if(nums[i]>0)
+    //             positive.push_back(nums[i]);
+    //         else 
+    //             negative.push_back(nums[i]);
+    //     }
+    //     int p=0,n=0;
+    //     for(int i=0;i<len;i++){
+    //         nums[i++]=positive[p++];
+    //         nums[i]=negative[n++];
+    //     }
+    //     return nums;
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> positive;
-        vector<int> negative;
         int len=nums.size();
+        vector<int> res(len);
+        int pos=0,neg=1;
         for(int i=0;i<len;i++){
-            if(nums[i]>0)
-                positive.push_back(nums[i]);
-            else 
-                negative.push_back(nums[i]);
+            if(nums[i]>0){
+                res[pos]=nums[i];
+                pos+=2;
+            }
+            else{
+                res[neg]=nums[i];
+                neg+=2;
+            }
         }
-        int p=0,n=0;
-        for(int i=0;i<len;i++){
-            nums[i++]=positive[p++];
-            nums[i]=negative[n++];
-        }
-        return nums;
+        return res;
     }
 };
