aa45d2af4216370d34d7c473202293d254366460
Ujjwal Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-02-06 15:23:40 +0530

    Time: 122 ms (10.62%), Space: 108.5 MB (5.30%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+//         int len=gas.size();
+//         int fuel_rem=0;
+//         int total_fuel=0;
+//         int ans=0;
+//         for(int i=0;i<len;i++){
+//             total_fuel+=gas[i]-cost[i];
+//             fuel_rem+=gas[i]-cost[i];
+//             if(fuel_rem<0){
+//                 fuel_rem=0;
+//                 ans=i+1;
+//             }
+//         }
+//         return (total_fuel<0)?-1:ans;
+//     }
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int len=gas.size();
+        int start=0;
         int fuel_rem=0;
-        int total_fuel=0;
-        int ans=0;
+        int fuel_rec=0;
         for(int i=0;i<len;i++){
-            total_fuel+=gas[i]-cost[i];
             fuel_rem+=gas[i]-cost[i];
             if(fuel_rem<0){
+                fuel_rec+=fuel_rem;
+                start=i+1;
                 fuel_rem=0;
-                ans=i+1;
             }
         }
-        return (total_fuel<0)?-1:ans;
+        return (fuel_rec+fuel_rem)>=0?start:-1;
     }
 };
