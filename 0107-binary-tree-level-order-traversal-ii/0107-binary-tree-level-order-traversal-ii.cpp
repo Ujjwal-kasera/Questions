cdd1c253c4f9ab7765680e72bb96bb7b5618824a
Ujjwal Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-02-15 20:16:11 +0530

    Time: 8 ms (35.11%), Space: 12.5 MB (67.84%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -10,6 +10,30 @@
  * };
  */
 class Solution {
+    // vector<vector<int>> levelOrderBottom(TreeNode* root) {
+    //     vector<vector<int>> res;
+    //     if(root==NULL)
+    //         return res;
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     int len;
+    //     TreeNode* curr;
+    //     vector<int> temp;
+    //     while(!q.empty()){
+    //         len=q.size();
+    //         temp={};
+    //         while(len--){
+    //             curr=q.front();
+    //             q.pop();
+    //             if(curr->left)
+    //                 q.push(curr->left);
+    //             if(curr->right)
+    //                 q.push(curr->right);
+    //             temp.push_back(curr->val);
+    //         }
+    //         res.insert(res.begin(),temp);
+    //     }
+    //     return res;
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
         vector<vector<int>> res;
@@ -32,8 +56,9 @@ public:
                     q.push(curr->right);
                 temp.push_back(curr->val);
             }
-            res.insert(res.begin(),temp);
+            res.push_back(temp);
         }
+        reverse(res.begin(),res.end());
         return res;
     }
 };
