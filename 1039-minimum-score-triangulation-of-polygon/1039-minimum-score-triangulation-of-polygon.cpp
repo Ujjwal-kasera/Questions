5baf84f6e93714104cdca08e7fcc3491213ff50a
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-03-26 22:58:49 +0530

    Time: 12 ms (35.16%), Space: 8.5 MB (59.54%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -14,6 +14,7 @@ public:
     }
     */
     
+    /*
     //Memoziation
     int solve(vector<int>&v,int i,int j,vector<vector<int>>&dp){
         if(i+1==j){
@@ -30,9 +31,24 @@ public:
         dp[i][j]=ans;
         return dp[i][j];
     }
+    */
+    
+    // Tabulation
+    int solve(vector<int>&v){
+        int size=v.size();
+        vector<vector<int>> dp(size,vector<int>(size,0));
+        for(int i=size-1;i>=0;i--){
+            for(int j=i+2;j<size;j++){
+                int ans=INT_MAX;
+                for(int k=i+1;k<j;k++){
+                    ans=min(ans,v[i]*v[j]*v[k]+dp[i][k]+dp[k][j]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][size-1];
+    }
     int minScoreTriangulation(vector<int>& values) {
-        int size=values.size();
-        vector<vector<int>> dp(size,vector<int>(size,-1));
-        return solve(values,0,size-1,dp);
+        return solve(values);
     }
 };
