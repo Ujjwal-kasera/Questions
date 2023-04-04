de0ab4b38dcdaa4b7e066038d73cabc4f8ba4489
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-04-04 12:48:04 +0530

    Time: 36 ms (78.53%), Space: 10.2 MB (90.34%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
+    /*same Longest Substring Without Repeating Characters wla logic use kr rhe h
+    bs jo usme agr substring me found hua to hm ek ek characterr htate the isme 
+    pura ka pura substring empty kr dege aur phr se process start krege
+    aur count ko bda dege
+    */
     int partitionString(string s) {
-        int count=0;
+        int count=1;
         string res="";
         int str_len=s.size();
         int i=0;
@@ -15,6 +20,6 @@ public:
                 count++;
             }
         }
-        return ++count;
+        return count;
     }
 };
