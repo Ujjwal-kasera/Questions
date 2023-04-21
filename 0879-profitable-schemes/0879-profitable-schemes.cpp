3fbb4eece9bb6435276ef38fa97e597813b35302
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-04-21 21:38:49 +0530

    Time: 415 ms (12.26%), Space: 53.9 MB (20.28%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int len;
+    int mod=1e9+7;
+    int target;
+    int solve(int n, int index, int Profit, vector<int>& group, vector<int>& profit, vector<vector<vector<int>>>&dp){
+        if(index==len)
+            return (Profit>=target);
+        int incl=0;
+        if(dp[index][n][Profit]!=-1)
+            return dp[index][n][Profit];
+        if(n-group[index]>=0)
+            incl=solve(n-group[index],index+1,min(target,Profit+profit[index]),group,profit,dp);
+        int excl=solve(n,index+1,Profit,group,profit,dp);
+        dp[index][n][Profit]=(incl+excl)%mod;
+        return dp[index][n][Profit];
+    }
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        len=group.size();
+        target=minProfit;
+        vector<vector<vector<int>>>dp(len+1,vector<vector<int>>(n+1,vector<int>(minProfit+1,-1)));
+        return solve(n,0,0,group,profit,dp);
+    }
+};
