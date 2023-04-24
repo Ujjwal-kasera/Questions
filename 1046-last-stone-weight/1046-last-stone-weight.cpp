6490c200f49c68169a6de03dd0930e94df6aa9fc
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-04-24 20:00:35 +0530

    Time: 2 ms (49.25%), Space: 7.7 MB (41.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         priority_queue<int> h;
-        for(auto i:stones)
+        for(int i:stones)
             h.push(i);
         int x,y;
         while(h.size()>1){
@@ -13,8 +13,6 @@ public:
             if(y!=x)
                 h.push(y-x);
         }
-        if(h.size())
-            return h.top();
-        return 0;
+        return h.size()==0?0:h.top();
     }
 };
