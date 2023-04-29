a6e96da894371cd0f640b55a1afc92b6340ccc71
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-04-29 12:46:33 +0530

    Time: 49 ms (33.08%), Space: 10.9 MB (28.90%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     map<pair<int,int>,int> mp;
     /*
+        Recursion
     int solveRec(vector<int>& arr,int left,int right){
         if(left>=right)
             return 0;
@@ -12,6 +13,8 @@ public:
         return ans;
     }
     */
+    /*
+        Memoziation
     int solveMem(vector<int>& arr,int left,int right,vector<vector<int>>&dp){
         if(left>=right)
             return 0;
@@ -23,7 +26,10 @@ public:
         }
         return dp[left][right]=ans;
     }
-    int mctFromLeafValues(vector<int>& arr) {
+    */
+    
+    /* Tabulation */
+    int solveTab(vector<int>&arr){
         int len=arr.size();
         for(int i=0;i<len;i++){
             mp[{i,i}]=arr[i];
@@ -31,7 +37,35 @@ public:
                 mp[{i,j}]=max(arr[j],mp[{i,j-1}]);
             }
         }
+        vector<vector<int>>dp(len,vector<int>(len,0));
+        for(int left=len-1;left>=0;left--){
+            for(int right=left;right<len;right++){
+                if(left==right)
+                    continue;
+                else{
+                    int ans=INT_MAX;
+                    for(int i=left;i<right;i++){
+                        ans=min(ans,(mp[{left,i}]*mp[{i+1,right}]+dp[left][i]+dp[i+1][right]));
+                    }
+                    dp[left][right]=ans;
+                }
+            }
+        }
+        return dp[0][len-1];
+    }
+    int mctFromLeafValues(vector<int>& arr) {
+        /* For Recursion & Memoziation 
+        int len=arr.size();
+        for(int i=0;i<len;i++){
+            mp[{i,i}]=arr[i];
+            for(int j=i+1;j<len;j++){
+                mp[{i,j}]=max(arr[j],mp[{i,j-1}]);
+            }
+        }
+        return solveRec(arr,0,len-1);
         vector<vector<int>>dp(len,vector<int>(len,-1));
         return solveMem(arr,0,len-1,dp);
+        */
+        return solveTab(arr);
     }
 };
