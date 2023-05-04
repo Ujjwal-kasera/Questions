8dc8ca96d941faff00bf8cf4a009f51eb38b82bf
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-05-05 02:20:08 +0530

    Time: 183 ms (63.77%), Space: 77.3 MB (67.71%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,15 +3,11 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
         int len=nums.size();
-        bool flag=false;
         for(int i=0;i<len;i++){
-            if(mp.count(nums[i])>0 && i-mp[nums[i]]<=k){
-                    flag=true;
-                    break;
-            }
-            else
-                mp[nums[i]]=i;
+            if(mp.count(nums[i])>0 && i-mp[nums[i]]<=k)
+                    return true;
+            mp[nums[i]]=i;
         }
-        return flag;
+        return false;
     }
 };
