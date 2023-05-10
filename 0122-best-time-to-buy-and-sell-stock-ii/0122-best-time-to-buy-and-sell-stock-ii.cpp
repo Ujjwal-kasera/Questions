730d943f16b307a3bd522fc81fdcf3f68f765016
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-05-10 23:25:56 +0530

    Time: 19 ms (8.58%), Space: 17.3 MB (8.25%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -16,6 +16,7 @@ public:
         return profit;
     }
     */
+    /*
     int maxProfit(vector<int>& prices) {
         int len=prices.size();
         int profit=0;
@@ -26,5 +27,29 @@ public:
                 profit+=prices[i]-prices[i-1];
         }
         return profit;
+    }
+    */
+    int len;
+    int solve(vector<int>& prices,bool buy,int index,vector<vector<int>> &dp){
+        if(index>=len){
+            return 0;
+        }
+        if(dp[index][buy]!=-1)
+            return dp[index][buy];
+        int profit1,profit2;
+        if(buy){
+            profit1=-prices[index]+solve(prices,0,index+1,dp);
+            profit2=solve(prices,buy,index+1,dp);
+        }
+        else{
+            profit1=prices[index]+solve(prices,1,index+1,dp);
+            profit2=solve(prices,buy,index+1,dp);
+        }
+        return dp[index][buy]=max(profit1,profit2);
+    }
+    int maxProfit(vector<int>& prices) {
+        len=prices.size();
+        vector<vector<int>> dp(len,vector<int>(2,-1));
+        return solve(prices,1,0,dp);
     }
 };
