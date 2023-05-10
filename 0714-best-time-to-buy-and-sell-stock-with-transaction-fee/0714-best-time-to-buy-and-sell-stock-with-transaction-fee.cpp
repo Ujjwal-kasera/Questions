8d617c2b983b0c40e288c910360a370306794069
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-05-11 00:31:48 +0530

    Time: 260 ms (17.08%), Space: 91 MB (45.09%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,43 +3,43 @@ public:
     int len;
     int fees;
     //Memoziation
-    int solveMem(vector<int>& prices,int index,bool buy,vector<vector<int>> &dp){
-        if(index>=len){
-            return 0;
-        }
-        if(dp[index][buy]!=-1)
-            return dp[index][buy];
-        int profit1,profit2;
-        if(buy){
-            profit1=-prices[index]+solveMem(prices,index+1,0,dp);
-            profit2=solveMem(prices,index+1,buy,dp);
-        }
-        else{
-            profit1=prices[index]+solveMem(prices,index+1,1,dp)-fees;
-            profit2=solveMem(prices,index+1,buy,dp);
-        }
-        return dp[index][buy]=max(profit1,profit2);
-    }
+    // int solveMem(vector<int>& prices,int index,bool buy,vector<vector<int>> &dp){
+    //     if(index>=len){
+    //         return 0;
+    //     }
+    //     if(dp[index][buy]!=-1)
+    //         return dp[index][buy];
+    //     int profit1,profit2;
+    //     if(buy){
+    //         profit1=-prices[index]+solveMem(prices,index+1,0,dp);
+    //         profit2=solveMem(prices,index+1,buy,dp);
+    //     }
+    //     else{
+    //         profit1=prices[index]+solveMem(prices,index+1,1,dp)-fees;
+    //         profit2=solveMem(prices,index+1,buy,dp);
+    //     }
+    //     return dp[index][buy]=max(profit1,profit2);
+    // }
     
     // Tabulation
-    // int solveTab(vector<int> &prices){
-    //     vector<vector<int>>dp(len+1,vector<int>(5,0));
-    //     for(int index=len-1;index>=0;index--){
-    //         for(int buy=3;buy>=0;buy--){
-    //             int profit1,profit2;
-    //             if(!(buy&1)){
-    //                 profit1=-prices[index]+dp[index+1][buy+1];
-    //                 profit2=dp[index+1][buy];
-    //             }
-    //             else{
-    //                 profit1=prices[index]+dp[index+1][buy+1];
-    //                 profit2=dp[index+1][buy];
-    //             }
-    //             dp[index][buy]=max(profit1,profit2);
-    //         }
-    //     }
-    //     return dp[0][0];
-    // }
+    int solveTab(vector<int> &prices){
+        vector<vector<int>>dp(len+1,vector<int>(2,0));
+        for(int index=len-1;index>=0;index--){
+            for(int buy=1;buy>=0;buy--){
+                int profit1,profit2;
+                if(buy){
+                    profit1=-prices[index]+dp[index+1][0];
+                    profit2=dp[index+1][buy];
+                }
+                else{
+                    profit1=prices[index]+dp[index+1][1]-fees;
+                    profit2=dp[index+1][buy];
+                }
+                dp[index][buy]=max(profit1,profit2);
+            }
+        }
+        return dp[0][1];
+    }
     
     // Space Optimisation
     // int solveSpace(vector<int> &prices){
@@ -66,9 +66,9 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         fees=fee;
         len=prices.size();
-        vector<vector<int>> dp(len,vector<int>(2,-1));
-        return solveMem(prices,0,1,dp);
-        // return solveTab(prices);
+        // vector<vector<int>> dp(len,vector<int>(2,-1));
+        // return solveMem(prices,0,1,dp);
+        return solveTab(prices);
         // return solveSpace(prices);
     }
 };
