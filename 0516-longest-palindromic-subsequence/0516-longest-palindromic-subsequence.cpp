0e2314f2f9d0a75a5ada56ee3455b8a3ecf1bc56
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-05-23 02:50:59 +0530

    Time: 194 ms (25.95%), Space: 6.6 MB (97.91%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -27,37 +27,37 @@ public:
     // }
     
     // Tabulation
-    int solveTab(string s1,string s2){
+    // int solveTab(string s1,string s2){
+    //     int len=s1.size();
+    //     vector<vector<int>> dp(len+1,vector<int>(len+1,0));
+    //     for(int i=len-1;i>=0;i--){
+    //         for(int j=len-1;j>=0;j--){
+    //             if(s1[i]==s2[j])
+    //                 dp[i][j]=1+dp[i+1][j+1];
+    //             else
+    //                 dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+    
+    // Space Optimisation
+    int solveOptimised(string s1,string s2){
         int len=s1.size();
-        vector<vector<int>> dp(len+1,vector<int>(len+1,0));
+        vector<int> curr(len+1,0);
+        vector<int> next(len+1,0);
         for(int i=len-1;i>=0;i--){
             for(int j=len-1;j>=0;j--){
                 if(s1[i]==s2[j])
-                    dp[i][j]=1+dp[i+1][j+1];
+                    curr[j]=1+next[j+1];
                 else
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                    curr[j]=max(next[j],curr[j+1]);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return curr[0];
     }
     
-    // Space Optimisation
-    // int solveOptimised(string text1,string text2){
-    //     int len1=text1.size();
-    //     int len2=text2.size();
-    //     vector<int> curr(len2+1,0);
-    //     vector<int> next(len2+1,0);
-    //     for(int i=len1-1;i>=0;i--){
-    //         for(int j=len2-1;j>=0;j--){
-    //             if(text1[i]==text2[j])
-    //                 curr[j]=1+next[j+1];
-    //             else
-    //                 curr[j]=max(next[j],curr[j+1]);
-    //         }
-    //         next=curr;
-    //     }
-    //     return curr[0];
-    // }
     int longestPalindromeSubseq(string s) {
         // len=s.size();
         string str=s;
@@ -65,7 +65,7 @@ public:
         // vector<vector<int>> dp(len,vector<int>(len,-1));
         // return solveRec(str,s,0,0);
         // return solveMem(str,s,0,0,dp);
-        return solveTab(str,s);
-        // return solveOptimised(text1,text2);
+        // return solveTab(str,s);
+        return solveOptimised(str,s);
     }
 };
