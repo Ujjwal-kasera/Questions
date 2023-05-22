619d8348ff3f577ec6415f0c8f1b1e971e0e227f
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-05-23 02:38:37 +0530

    Time: 60 ms (65.73%), Space: 6.5 MB (91.78%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -27,19 +27,37 @@ public:
     // }
     
     // Tabulation
-    int solveTab(string text1,string text2){
+    // int solveTab(string text1,string text2){
+    //     int len1=text1.size();
+    //     int len2=text2.size();
+    //     vector<vector<int>> dp(len1+1,vector<int>(len2+1,0));
+    //     for(int i=len1-1;i>=0;i--){
+    //         for(int j=len2-1;j>=0;j--){
+    //             if(text1[i]==text2[j])
+    //                 dp[i][j]=1+dp[i+1][j+1];
+    //             else
+    //                 dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+    
+    // Space Optimisation
+    int solveOptimised(string text1,string text2){
         int len1=text1.size();
         int len2=text2.size();
-        vector<vector<int>> dp(len1+1,vector<int>(len2+1,0));
+        vector<int> curr(len2+1,0);
+        vector<int> next(len2+1,0);
         for(int i=len1-1;i>=0;i--){
             for(int j=len2-1;j>=0;j--){
                 if(text1[i]==text2[j])
-                    dp[i][j]=1+dp[i+1][j+1];
+                    curr[j]=1+next[j+1];
                 else
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                    curr[j]=max(next[j],curr[j+1]);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return curr[0];
     }
     
     int longestCommonSubsequence(string text1, string text2) {
@@ -48,6 +66,7 @@ public:
         // vector<vector<int>> dp(len1,vector<int>(len2,-1));
         // return solveRec(text1,text2,0,0);
         // return solveMem(text1,text2,0,0,dp);
-        return solveTab(text1,text2);
+        // return solveTab(text1,text2);
+        return solveOptimised(text1,text2);
     }
 };
