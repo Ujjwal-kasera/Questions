771ed9e6168410adfc942fec34b971db5262dc15
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-05 13:16:57 +0530

    Time: 60 ms (6.87%), Space: 36.4 MB (98.32%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -5,13 +5,16 @@ public:
         int zero=0;
         int ans=0;
         int start=0;
+        int temp=0;
         for(int i=0;i<len;i++){
             zero+=(nums[i]==0);
             while(zero>1){
                 zero-=(nums[start]==0);
                 start++;
             }
-            ans=max(ans,i-start);
+            temp=i-start;
+            if(temp>ans)
+                ans=temp;
         }
         return ans;
     }
