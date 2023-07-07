3b01a58a1d48fadaf57c48c093f020527f549b25
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-07 12:18:16 +0530

    Time: 13 ms (70.31%), Space: 8.5 MB (94.01%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
+    // string removeKdigits(string num, int k) {
+    //     int len=num.size();
+    //     stack<char>st;
+    //     char ch;
+    //     for(int i=0;i<len;i++){
+    //         ch=num[i];
+    //         while(k>0 && !st.empty() && st.top()>ch){
+    //             st.pop();
+    //             k--;
+    //         }
+    //         st.push(ch);
+    //         if(st.size() == 1 && ch == '0')
+    //             st.pop();
+    //     }
+    //     while(k>0 && !st.empty()){
+    //         st.pop();
+    //         k--;
+    //     }
+    //     num="";
+    //     while(!st.empty()){
+    //         num+=st.top();
+    //         st.pop();
+    //     }
+    //     reverse(num.begin(),num.end());
+    //     return num==""?"0":num;
+    
+    // More Optimised solution
     string removeKdigits(string num, int k) {
         int len=num.size();
-        stack<char>st;
+        string res;
         char ch;
         for(int i=0;i<len;i++){
             ch=num[i];
-            while(k>0 && !st.empty() && st.top()>ch){
-                st.pop();
+            while(k>0 && !res.empty() && res.back()>ch){
+                res.pop_back();
                 k--;
             }
-            st.push(ch);
-            if(st.size() == 1 && ch == '0')
-                st.pop();
+            res+=ch;
+            if(res.size() == 1 && ch == '0')
+                res.pop_back();
         }
-        while(k>0 && !st.empty()){
-            st.pop();
+        while(k>0 && res.size()>0){
+            res.pop_back();
             k--;
         }
-        num="";
-        while(!st.empty()){
-            num+=st.top();
-            st.pop();
-        }
-        reverse(num.begin(),num.end());
-        return num==""?"0":num;
+        return res==""?"0":res;
     }
 };
