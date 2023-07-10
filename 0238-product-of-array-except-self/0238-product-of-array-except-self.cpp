86ca82fe079bd82ca984c0c0dd6bc94274404913
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-11 01:11:14 +0530

    Time: 31 ms (48.28%), Space: 24.1 MB (52.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,15 +3,12 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int len=nums.size();
         vector<int> vec(len,1);
-        int pro=1;
+        int pro1=1,pro2=1;
         for(int i=0;i<len;++i){
-            vec[i]*=pro;
-            pro*=nums[i];
-        }
-        pro=1;
-        for(int i=len-1;i>=0;--i){
-            vec[i]*=pro;
-            pro*=nums[i];
+            vec[i]*=pro1;
+            pro1*=nums[i];
+            vec[len-i-1]*=pro2;
+            pro2*=nums[len-i-1];
         }
         return vec;
     }
