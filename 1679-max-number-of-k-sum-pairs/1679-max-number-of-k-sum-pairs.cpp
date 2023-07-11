903889e6503f86ba9f73ab392c53feefbdbbeb70
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-12 00:31:04 +0530

    Time: 194 ms (31.94%), Space: 68.1 MB (25.04%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,52 +1,20 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        int res=0;
-        int tmp;
         map<int,int> mp;
         int len=nums.size();
         for(int i=0;i<len;i++){
-            tmp=k-nums[i];
-            if(mp[tmp]>0){
-                res++;
-                mp[tmp]--;
-            }
-            else
-                mp[nums[i]]++;
+            mp[nums[i]]++;
         }
-        return res;
+        int res=0;
+        int num,count;
+        for(auto i=mp.begin();i!=mp.end();++i){
+            num=i->first;
+            count=i->second;
+            if(mp.count(k-num)){
+                res+=min(count,mp[k-num]);
+            }
+        }
+        return (res>>1);
     }
-    
-    
-//     int maxOperations(vector<int>& nums, int k) {
-//         map<int,int> mp;
-//         int len=nums.size();
-//         for(int i=0;i<len;i++){
-//             mp[nums[i]]++;
-//         }
-//         int res=0;
-//         int num,count;
-//         for(auto i=mp.begin();i!=mp.end();++i){
-//             num=i->first;
-//             count=i->second;
-//             if(mp.count(k-num)){
-//                 res+=min(count,mp[k-num]);
-//             }
-//         }
-//         return (res>>1);
-//     }
-    
-    
-//     int maxOperations(vector<int>& nums, int k) {
-//         map<int,int> mp;
-//         int len=nums.size();
-//         for(int i=0;i<len;i++){
-//             mp[nums[i]]++;
-//         }
-//         int count=0;
-//         for(auto i:mp){
-//             count+=min(i.second,mp[k-i.first]);
-//         }
-//         return floor(count/2);
-//     }
 };
