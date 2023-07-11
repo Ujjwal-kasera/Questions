d91077d5f4d6e320489763a5294b38eebd26d195
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-12 00:26:55 +0530

    Time: 227 ms (13.88%), Space: 68.1 MB (26.35%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -6,11 +6,28 @@ public:
         for(int i=0;i<len;i++){
             mp[nums[i]]++;
         }
-        int count=0;
-        for(auto i:mp){
-            count+=min(i.second,mp[k-i.first]);
+        int res=0;
+        int num,count;
+        for(auto i=mp.begin();i!=mp.end();++i){
+            num=i->first;
+            count=i->second;
+            if(mp.count(k-num)){
+                res+=min(count,mp[k-num]);
+            }
         }
-        return floor(count/2);
+        return (res>>1);
         
     }
+//     int maxOperations(vector<int>& nums, int k) {
+//         map<int,int> mp;
+//         int len=nums.size();
+//         for(int i=0;i<len;i++){
+//             mp[nums[i]]++;
+//         }
+//         int count=0;
+//         for(auto i:mp){
+//             count+=min(i.second,mp[k-i.first]);
+//         }
+//         return floor(count/2);
+//     }
 };
