e2e36cfb2c8384935e0831290ee92bf885ca2841
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-17 12:10:33 +0530

    Time: 17 ms (67.14%), Space: 17.1 MB (27.00%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,28 +1,5 @@
 class Solution {
 public:
-    // Approach 1
-    // Insert the given new interval in the interval array..sort the array
-    // And Now merge the array 
-    // vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-    //     int len=intervals.size();
-    //     intervals.push_back(newInterval);
-    //     vector<vector<int>> res;
-    //     sort(intervals.begin(),intervals.end());
-    //     int start=intervals[0][0],end=intervals[0][1];
-    //     for(int i=1;i<=len;++i){
-    //         if(intervals[i][0]<=end){
-    //             end=max(end,intervals[i][1]);
-    //             continue;
-    //         }
-    //         else{
-    //             res.push_back({start,end});
-    //             start=intervals[i][0];
-    //             end=intervals[i][1];
-    //         }
-    //     }
-    //     res.push_back({start,end});
-    //     return res;
-    // }
     
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int len=intervals.size();
