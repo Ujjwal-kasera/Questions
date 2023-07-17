dd49f64bbef85fdfab92b69b04aa0e8e12eb8605
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-17 12:10:17 +0530

    Time: 23 ms (39.07%), Space: 17.1 MB (49.39%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
+    // Approach 1
+    // Insert the given new interval in the interval array..sort the array
+    // And Now merge the array 
+    // vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    //     int len=intervals.size();
+    //     intervals.push_back(newInterval);
+    //     vector<vector<int>> res;
+    //     sort(intervals.begin(),intervals.end());
+    //     int start=intervals[0][0],end=intervals[0][1];
+    //     for(int i=1;i<=len;++i){
+    //         if(intervals[i][0]<=end){
+    //             end=max(end,intervals[i][1]);
+    //             continue;
+    //         }
+    //         else{
+    //             res.push_back({start,end});
+    //             start=intervals[i][0];
+    //             end=intervals[i][1];
+    //         }
+    //     }
+    //     res.push_back({start,end});
+    //     return res;
+    // }
+    
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int len=intervals.size();
-        intervals.push_back(newInterval);
+        int index=0;
         vector<vector<int>> res;
-        sort(intervals.begin(),intervals.end());
-        int start=intervals[0][0],end=intervals[0][1];
-        for(int i=1;i<=len;++i){
-            if(intervals[i][0]<=end){
-                end=max(end,intervals[i][1]);
-                continue;
-            }
-            else{
-                res.push_back({start,end});
-                start=intervals[i][0];
-                end=intervals[i][1];
-            }
+        int start=newInterval[0],end=newInterval[1];
+        while(index<len && intervals[index][1]<start)
+            res.push_back(intervals[index++]);
+        
+        while(index<len && intervals[index][0]<=end){
+            start=min(start,intervals[index][0]);
+            end=max(end,intervals[index++][1]);
         }
         res.push_back({start,end});
+        
+        while(index<len)
+            res.push_back(intervals[index++]);
+        
         return res;
     }
 };
