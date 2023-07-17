2d1252c8c8cac4ec778306d30a79d5623b6f6b80
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-17 12:12:24 +0530

    Time: 19 ms (60.35%), Space: 17 MB (82.27%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    // Approach 1
+    // Insert the given new interval in the interval array..sort the array
+    // And Now merge the array 
+    // vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    //     int len=intervals.size();
+    //     intervals.push_back(newInterval);
+    //     vector<vector<int>> res;
+    //     sort(intervals.begin(),intervals.end());
+    //     int start=intervals[0][0],end=intervals[0][1];
+    //     for(int i=1;i<=len;++i){
+    //         if(intervals[i][0]<=end){
+    //             end=max(end,intervals[i][1]);
+    //             continue;
+    //         }
+    //         else{
+    //             res.push_back({start,end});
+    //             start=intervals[i][0];
+    //             end=intervals[i][1];
+    //         }
+    //     }
+    //     res.push_back({start,end});
+    //     return res;
+    // }
     
+    // Apporach 2
+    // pichle approach me hm jo array me insert ke ke phr se sort kr rhe the use hm optimise krege
+    // kyu ki hmra jo interval newinterval se intersect kr rha whi us change hoga baki nhi hoga 
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int len=intervals.size();
         int index=0;
