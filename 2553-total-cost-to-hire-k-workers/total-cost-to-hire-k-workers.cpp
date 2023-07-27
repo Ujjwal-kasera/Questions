82e0fe2e7a32bce4563017c9681131ac1b91698e
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-28 00:34:22 +0530

    Time: 108 ms (99.95%) | Memory: 72.6 MB (93.86%) - LeetSync

---
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,44 @@
+#define ll long long
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int,vector<int>,greater<int>> first;
+        priority_queue<int,vector<int>,greater<int>> last;
+        int len=costs.size();
+        int inF=0,inL=len-1;
+        for(int i=0;i<candidates;i++){
+            if(inF<=inL)
+                first.push(costs[inF++]);
+            if(inF<=inL)
+                last.push(costs[inL--]);
+        }
+        ll cost=0;
+        while(k--){
+            if(first.empty()){
+                cost+=last.top();
+                last.pop();
+                if(inF<=inL)
+                    last.push(costs[inL--]);
+            }
+            else if(last.empty()){
+                cost+=first.top();
+                first.pop();
+                if(inF<=inL)
+                    first.push(costs[inF++]);
+            }
+            else if(first.top()<=last.top()){
+                cost+=first.top();
+                first.pop();
+                if(inF<=inL)
+                    first.push(costs[inF++]);
+            }
+            else{
+                cost+=last.top();
+                last.pop();
+                if(inF<=inL)
+                    last.push(costs[inL--]);
+            }
+        }
+        return cost;
+    }
+};
