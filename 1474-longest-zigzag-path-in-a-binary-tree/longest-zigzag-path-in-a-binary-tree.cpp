fa9efe1d8ae572d125ce46e9cdadbbfd130bf699
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-27 23:09:20 +0530

    Time: 115 ms (99.77%) | Memory: 94.1 MB (54.02%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -11,28 +11,50 @@
  */
 class Solution {
 public:
+    
     int ans=0;
-    int path(TreeNode* root,int count,bool dir){
-        if(!root)
-            return count-1;
-        int ans;
-        if(!dir)
-            ans=path(root->right,count+1,!dir);
-        else
-            ans=path(root->left,count+1,!dir);
-        return ans;
-    }
-    void traverse(TreeNode* root){
+    void path(TreeNode* root,int count,bool dir){
         if(!root)
             return;
-        int temp1=path(root,0,0);
-        int temp2=path(root,0,1);
-        ans=max({ans,temp1,temp2});
-        traverse(root->left);
-        traverse(root->right);
+        ans=max(ans,count);
+        if(dir==0){
+            path(root->left,count+1,1);
+            path(root->right,1,0);
+        }
+        else{
+            path(root->right,count+1,0);
+            path(root->left,1,1);
+        }
+        return;
     }
     int longestZigZag(TreeNode* root) {
-        traverse(root);
+        path(root,0,0);
+        path(root,0,1);
         return ans;
     }
+    
+    // int ans=0;
+    // int path(TreeNode* root,int count,bool dir){
+    //     if(!root)
+    //         return count-1;
+    //     int ans;
+    //     if(!dir)
+    //         ans=path(root->right,count+1,!dir);
+    //     else
+    //         ans=path(root->left,count+1,!dir);
+    //     return ans;
+    // }
+    // void traverse(TreeNode* root){
+    //     if(!root)
+    //         return;
+    //     int temp1=path(root,0,0);
+    //     int temp2=path(root,0,1);
+    //     ans=max({ans,temp1,temp2});
+    //     traverse(root->left);
+    //     traverse(root->right);
+    // }
+    // int longestZigZag(TreeNode* root) {
+    //     traverse(root);
+    //     return ans;
+    // }
 };
