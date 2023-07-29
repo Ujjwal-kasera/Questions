ca268724bbcbdbdc72b2fbe0b79b999870e0bbab
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-29 18:34:25 +0530

    Time: 3 ms (46.16%) | Memory: 6.2 MB (98.42%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void solve(int index,int k,int n,vector<int> vec){
-        if(k==0 ||  n==0){
-            if(k==0 && n==0)
+    void solve(int index,int k,int n,vector<int> &vec){
+        if(!k || !n){
+            if(!k && !n)
             ans.push_back(vec);
             return;
         }
@@ -16,7 +16,27 @@ public:
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        solve(1,k,n,{});
+        vector<int> tmp;
+        solve(1,k,n,tmp);
         return ans;
     }
+    
+    // void solve(int index,int k,int n,vector<int> vec){
+    //     if(k==0 ||  n==0){
+    //         if(k==0 && n==0)
+    //         ans.push_back(vec);
+    //         return;
+    //     }
+    //     for(int i=index;i<10;i++){
+    //         if(i>n)
+    //             return;
+    //         vec.push_back(i);
+    //         solve(i+1,k-1,n-i,vec);
+    //         vec.pop_back();
+    //     }
+    // }
+    // vector<vector<int>> combinationSum3(int k, int n) {
+    //     solve(1,k,n,{});
+    //     return ans;
+    // }
 };
