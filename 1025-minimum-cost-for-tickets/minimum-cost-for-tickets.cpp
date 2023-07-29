25720d2f58b948eae7acc68e7d5ac5ef0edc50fb
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-29 23:46:22 +0530

    Time: 6 ms (39.07%) | Memory: 9.6 MB (47.63%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int len;
+    // int solveRec(int index,vector<int>& days, vector<int>& costs){
+    //     if(index>=len)
+    //         return 0;
+    //     int day1=costs[0]+solveRec(index+1,days,costs);
+    //     int idx;
+    //     for(idx=index;idx<len && days[idx]<days[index]+7;++idx);
+    //     int day7=costs[1]+solveRec(idx,days,costs);
+    //     for(idx=index;idx<len && days[idx]<days[index]+30;++idx);
+    //     int day30=costs[2]+solveRec(idx,days,costs);
+    //     return min({day1,day7,day30});
+    // }
+
+    int solveMem(int index,vector<int>& days, vector<int>& costs,vector<int>&dp){
+        if(index>=len)
+            return 0;
+        if(dp[index]!=-1)
+            return dp[index];
+        int day1=costs[0]+solveMem(index+1,days,costs,dp);
+        int idx;
+        for(idx=index;idx<len && days[idx]<days[index]+7;++idx);
+        int day7=costs[1]+solveMem(idx,days,costs,dp);
+        for(idx=index;idx<len && days[idx]<days[index]+30;++idx);
+        int day30=costs[2]+solveMem(idx,days,costs,dp);
+        return dp[index]=min({day1,day7,day30});
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        len=days.size();
+        vector<int> dp(len,-1);
+        return solveMem(0,days,costs,dp);
+        // return solveRec(0,days,costs);
+    }
+};
