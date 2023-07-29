a6bcb779ec8f9bf0154ee353fb47bf597615ca88
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-07-29 23:50:06 +0530

    Time: 6 ms (39.07%) | Memory: 9.6 MB (77.35%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -13,23 +13,38 @@ public:
     //     return min({day1,day7,day30});
     // }
 
-    int solveMem(int index,vector<int>& days, vector<int>& costs,vector<int>&dp){
-        if(index>=len)
-            return 0;
-        if(dp[index]!=-1)
-            return dp[index];
-        int day1=costs[0]+solveMem(index+1,days,costs,dp);
-        int idx;
-        for(idx=index;idx<len && days[idx]<days[index]+7;++idx);
-        int day7=costs[1]+solveMem(idx,days,costs,dp);
-        for(idx=index;idx<len && days[idx]<days[index]+30;++idx);
-        int day30=costs[2]+solveMem(idx,days,costs,dp);
-        return dp[index]=min({day1,day7,day30});
+    // int solveMem(int index,vector<int>& days, vector<int>& costs,vector<int>&dp){
+    //     if(index>=len)
+    //         return 0;
+    //     if(dp[index]!=-1)
+    //         return dp[index];
+    //     int day1=costs[0]+solveMem(index+1,days,costs,dp);
+    //     int idx;
+    //     for(idx=index;idx<len && days[idx]<days[index]+7;++idx);
+    //     int day7=costs[1]+solveMem(idx,days,costs,dp);
+    //     for(idx=index;idx<len && days[idx]<days[index]+30;++idx);
+    //     int day30=costs[2]+solveMem(idx,days,costs,dp);
+    //     return dp[index]=min({day1,day7,day30});
+    // }
+
+    int solveTab(vector<int>& days, vector<int>& costs){
+        vector<int> dp(len+1,0);
+        for(int index=len-1;index>=0;--index){
+            int day1=costs[0]+dp[index+1];
+            int idx;
+            for(idx=index;idx<len && days[idx]<days[index]+7;++idx);
+            int day7=costs[1]+dp[idx];
+            for(idx=index;idx<len && days[idx]<days[index]+30;++idx);
+            int day30=costs[2]+dp[idx];
+            dp[index]=min({day1,day7,day30});
+        }
+        return dp[0];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         len=days.size();
-        vector<int> dp(len,-1);
-        return solveMem(0,days,costs,dp);
+        return solveTab(days,costs);
+        // vector<int> dp(len,-1);
+        // return solveMem(0,days,costs,dp);
         // return solveRec(0,days,costs);
     }
 };
