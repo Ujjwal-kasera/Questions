c39badc1acddee4f3f44de8135d1c198df725782
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-03 00:35:36 +0530

    Time: 99 ms (21.21%) | Memory: 5.9 MB (81.22%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-    int k;
-    string ans;
-    void solve(vector<bool> &visited,string s,int n,int count){
-        if(count==0){
-            k--;
-            if(k==0)
-                ans=s;
-            return;
-        }
-        for(int i=1;i<=n;i++){
-            if(!visited[i]){
-                visited[i]=1;
-                s+=(i+'0');
-                solve(visited,s,n,count-1);
-                s.pop_back();
-                visited[i]=0;
-            }
-        }
-    }
+    // int k;
+    // string ans;
+    // void solve(vector<bool> &visited,string s,int n,int count){
+    //     if(count==0){
+    //         k--;
+    //         if(k==0)
+    //             ans=s;
+    //         return;
+    //     }
+    //     for(int i=1;i<=n;i++){
+    //         if(!visited[i]){
+    //             visited[i]=1;
+    //             s+=(i+'0');
+    //             solve(visited,s,n,count-1);
+    //             s.pop_back();
+    //             visited[i]=0;
+    //         }
+    //     }
+    // }
+    // string getPermutation(int n, int k) {
+    //     this->k=k;
+    //     vector<bool>visited(n+1,0);
+    //     string s;
+    //     solve(visited,s,n,n);
+    //     return ans;
+    // }
+    
+    // Using STL funtion next_permutaion
     string getPermutation(int n, int k) {
-        this->k=k;
-        vector<bool>visited(n+1,0);
+        vector<int> vec;
+        for(int i=0;i<n;i++) vec.push_back(i+1);
+        for(int i=0;i<k-1;i++) next_permutation(vec.begin(),vec.end());
         string s;
-        solve(visited,s,n,n);
-        return ans;
+        for(int i=0;i<n;i++) s+='0'+vec[i];
+        return s;
     }
 };
