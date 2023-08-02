05e8baa9efdf1ec17b676a16b61dc6bdf8d9896e
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-02 23:01:55 +0530

    Time: 98 ms (43.56%) | Memory: 22.6 MB (23.33%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     int len,count=0,total=0,tar;
-    int solveRec(vector<int>&nums,int t,int index,vector<vector<int>> &dp){
-        if(index>=len){
-            if(t==tar)
-                return 1;
-            return 0;
+
+    // Memoziation
+    // int solveMem(vector<int>&nums,int t,int index,vector<vector<int>> &dp){
+    //     if(index>=len){
+    //         if(t==tar)
+    //             return 1;
+    //         return 0;
+    //     }
+    //     if(dp[index][t+total]!=-1)
+    //         return dp[index][t+total];
+    //     int plus=solveMem(nums,t-nums[index],index+1,dp);
+    //     int minus=solveMem(nums,t+nums[index],index+1,dp);
+    //     return dp[index][t+total]=plus+minus;
+    // }
+
+    // Tabulation
+    int solveTab(vector<int>&nums){
+        vector<vector<int>> dp(len+1,vector<int>(2*total+1,0));
+        dp[len][tar+total]=1;
+        for(int index=len-1;index>=0;--index){
+            for(int t=total;t>=-total;t--){
+                if((t-nums[index]+total)>=0  && (t-nums[index]+total)<=2*total)
+                    dp[index][t+total]+=dp[index+1][t-nums[index]+total];
+                if((t+nums[index]+total)>=0  && (t+nums[index]+total)<=2*total)
+                    dp[index][t+total]+=dp[index+1][t+nums[index]+total];
+            }
         }
-        if(dp[index][t+total]!=-1)
-            return dp[index][t+total];
-        int plus=solveRec(nums,t-nums[index],index+1,dp);
-        int minus=solveRec(nums,t+nums[index],index+1,dp);
-        return dp[index][t+total]=plus+minus;
+        return dp[0][0+total];
     }
+    
     int findTargetSumWays(vector<int>& nums, int target) {
         len=nums.size();
         tar=target;
         for(auto ele:nums)
             total+=ele;
-        vector<vector<int>> dp(len,vector<int>(2*total+1,-1));
-        return solveRec(nums,0,0,dp);
+        if(abs(target)>total )
+            return 0;
+        return solveTab(nums);
+        // vector<vector<int>> dp(len,vector<int>(2*total+1,-1));
+        // return solveMem(nums,0,0,dp);
     }
 };
