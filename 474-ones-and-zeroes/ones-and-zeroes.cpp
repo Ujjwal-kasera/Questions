1b31452c4481f74013313242694ea56ab39c120b
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-04 17:20:41 +0530

    Time: 254 ms (75.91%) | Memory: 100.1 MB (49.43%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public: 
     int one_max,zero_max,len;
-    unordered_map<string,pair<int,int>> mp;
     int solveMemo(vector<string>&strs,int one,int zero,int index,vector<vector<vector<int>>> &dp){
         if(index>=len || one>one_max || zero>zero_max)
             return 0;
-        
+
         if(dp[index][one][zero]!=-1)
             return dp[index][one][zero];
-
+        int zeroes=count(strs[index].begin(),strs[index].end(),'0');
+        int ones=strs[index].size()-zeroes;
         int incl=0;
-        if((one+mp[strs[index]].first<=one_max) && (zero+mp[strs[index]].second<=zero_max))
-            incl=1+solveMemo(strs,one+mp[strs[index]].first,zero+mp[strs[index]].second,index+1,dp);
+        if((one+ones<=one_max) && (zero+zeroes<=zero_max))
+            incl=1+solveMemo(strs,one+ones,zero+zeroes,index+1,dp);
         int excl=solveMemo(strs,one,zero,index+1,dp);
         dp[index][one][zero]=max(incl,excl);
         return dp[index][one][zero];
@@ -21,12 +21,6 @@ public:
         len=strs.size();
         one_max=n;
         zero_max=m;
-        int one,zero,tmp;
-        for(int i=0;i<len;i++){
-            zero=count(strs[i].begin(),strs[i].end(),'0');
-            one=strs[i].size()-zero;
-            mp.insert({strs[i],{one,zero}});
-        }
         vector<vector<vector<int>>> dp(len,vector<vector<int>>(n+1,vector<int>(m+1,-1)));
         return solveMemo(strs,0,0,0,dp);
     }
