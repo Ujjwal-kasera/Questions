b6b8821b7aac6013d6a83952954530f57f40bd3a
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-07 00:40:10 +0530

    Time: 295 ms (94.99%) | Memory: 8.1 MB (38.82%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int len,rowLen,colLen;
+    string word;
     vector<vector<bool>> vis;
-    bool isVaid(vector<vector<char>> &board,string &word,int row,int col,int idx){
+    vector<vector<char>> board;
+    
+    bool isVaid(int row,int col,int idx){
         if(row>=0 && row<rowLen && col>=0 && col<colLen && board[row][col]==word[idx] && vis[row][col]==0)
             return 1;
         return 0;
     }
-    bool solve(vector<vector<char>>&board,string &word,int row, int col, int idx){
+    bool solve(int row, int col, int idx){
         if(idx==len-1)
             return 1;
 
@@ -18,20 +21,20 @@ public:
         vis[row][col]=1;
 
         // Up
-        if(isVaid(board,word,row-1,col,idx+1))
-            ans|=solve(board,word,row-1,col,idx+1);
+        if(isVaid(row-1,col,idx+1))
+            ans|=solve(row-1,col,idx+1);
 
         // Down
-        if(isVaid(board,word,row+1,col,idx+1))
-            ans|=solve(board,word,row+1,col,idx+1);
+        if(isVaid(row+1,col,idx+1))
+            ans|=solve(row+1,col,idx+1);
 
         // Left
-        if(isVaid(board,word,row,col-1,idx+1))
-            ans|=solve(board,word,row,col-1,idx+1);
+        if(isVaid(row,col-1,idx+1))
+            ans|=solve(row,col-1,idx+1);
 
         // Right
-        if(isVaid(board,word,row,col+1,idx+1))
-            ans|=solve(board,word,row,col+1,idx+1);
+        if(isVaid(row,col+1,idx+1))
+            ans|=solve(row,col+1,idx+1);
 
         vis[row][col]=0;
         return ans;
@@ -40,17 +43,17 @@ public:
         len=word.size();
         rowLen=board.size();
         colLen=board[0].size();
+        this->word=word;
+        this->board=board;
         vis.assign(rowLen,vector<bool>(colLen,0));
-        bool ans=false;
         for(int row=0;row<rowLen;++row){
             for(int col=0;col<colLen;++col){
                 if(board[row][col]==word[0]){
-                    ans=solve(board,word,row,col,0);
-                    if(ans==true)
-                        return ans;
+                    if(solve(row,col,0))
+                        return true;
                 }
             }
         }
-        return ans;
+        return false;
     }
 };
