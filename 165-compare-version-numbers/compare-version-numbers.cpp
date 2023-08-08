0b388d42647a64225610e94616ea42a6d3ead476
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-08 19:35:09 +0530

    Time: 2 ms (38.34%) | Memory: 6.2 MB (24.62%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -14,9 +14,9 @@ public:
                 ver2*=10;
                 ver2+=(version2[ptr2++]-'0');
             }
-            if(version1[ptr1]=='.')
+            if(ptr1<len1 && version1[ptr1]=='.')
                 ptr1++;
-            if(version2[ptr2]=='.')
+            if(ptr2<len2 && version2[ptr2]=='.')
                 ptr2++;
             if(ver1>ver2)
                 return 1;
