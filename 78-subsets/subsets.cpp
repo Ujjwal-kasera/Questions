2e425de031a5f7772762821bd16fd75ee99e857a
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-09 16:08:51 +0530

    Time: 2 ms (61.70%) | Memory: 7.1 MB (61.00%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,18 +2,28 @@ class Solution {
 public:
     int len;
     vector<vector<int>> res;
-    void backtrack(vector<int>&nums,vector<int>&temp,int idx){
+    void backTrack(vector<int>&nums,vector<int>&temp,int idx){
             res.push_back(temp);
         for(int i=idx;i<len;i++){
             temp.push_back(nums[i]);
-            backtrack(nums,temp,i+1);
+            backTrack(nums,temp,i+1);
             temp.pop_back();
+        }
+    }
+    void bitMunipulation(vector<int>&nums,vector<int>&temp){
+        for(int i=0;i<(1<<len);i++){
+            temp.clear();
+            for(int j=0;j<len;j++)
+                if(i&(1<<j))
+                    temp.push_back(nums[j]);
+            res.push_back(temp);
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         len=nums.size();
         vector<int> temp;
-        backtrack(nums,temp,0);
+        // backTrack(nums,temp,0);
+        bitMunipulation(nums,temp);
         return res;
     }
 };
