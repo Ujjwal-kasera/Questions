174249c1a1ddc2f1022eb2ba3c08d32f1079f785
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-09 18:03:24 +0530

    Time: 3 ms (67.76%) | Memory: 8 MB (39.16%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int len;
+    vector<int> temp;
     vector<vector<int>> res;
+
+    // Using Backtracking
+    // void backTrack(vector<int>&nums,int idx){
+    //     res.push_back(temp);
+    //     for(int i=idx;i<len;i++){
+    //         if(i!=idx && nums[i-1]==nums[i])
+    //             continue;
+    //         temp.push_back(nums[i]);
+    //         backTrack(nums,i+1);
+    //         temp.pop_back();
+    //     }
+    // }
 
     // Using Bit Manipulation
     void bitManipulation(vector<int>&nums){
-        vector<int> temp;
         set<vector<int>> st;
         for(int i=0;i<(1<<len);i++){
             temp.clear();
@@ -21,6 +33,8 @@ public:
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         len=nums.size();
+        sort(nums.begin(),nums.end());
+        // backTrack(nums,0);
         bitManipulation(nums);
         return res;
     }
