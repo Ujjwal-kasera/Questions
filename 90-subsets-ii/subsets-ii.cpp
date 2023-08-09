00ccfdc8db4665e3a276287447cee3e2c04ca9df
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-09 18:03:36 +0530

    Time: 4 ms (54.48%) | Memory: 7.7 MB (50.08%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -5,37 +5,37 @@ public:
     vector<vector<int>> res;
 
     // Using Backtracking
-    // void backTrack(vector<int>&nums,int idx){
-    //     res.push_back(temp);
-    //     for(int i=idx;i<len;i++){
-    //         if(i!=idx && nums[i-1]==nums[i])
-    //             continue;
-    //         temp.push_back(nums[i]);
-    //         backTrack(nums,i+1);
-    //         temp.pop_back();
-    //     }
-    // }
+    void backTrack(vector<int>&nums,int idx){
+        res.push_back(temp);
+        for(int i=idx;i<len;i++){
+            if(i!=idx && nums[i-1]==nums[i])
+                continue;
+            temp.push_back(nums[i]);
+            backTrack(nums,i+1);
+            temp.pop_back();
+        }
+    }
 
     // Using Bit Manipulation
-    void bitManipulation(vector<int>&nums){
-        set<vector<int>> st;
-        for(int i=0;i<(1<<len);i++){
-            temp.clear();
-            for(int j=0;j<len;j++)
-                if(i&(1<<j))
-                    temp.push_back(nums[j]);
-            sort(temp.begin(),temp.end());
-            st.insert(temp);
-        }
-        for(auto vec:st)
-            res.push_back(vec);
-    }
+    // void bitManipulation(vector<int>&nums){
+    //     set<vector<int>> st;
+    //     for(int i=0;i<(1<<len);i++){
+    //         temp.clear();
+    //         for(int j=0;j<len;j++)
+    //             if(i&(1<<j))
+    //                 temp.push_back(nums[j]);
+    //         sort(temp.begin(),temp.end());
+    //         st.insert(temp);
+    //     }
+    //     for(auto vec:st)
+    //         res.push_back(vec);
+    // }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         len=nums.size();
         sort(nums.begin(),nums.end());
-        // backTrack(nums,0);
-        bitManipulation(nums);
+        backTrack(nums,0);
+        // bitManipulation(nums);
         return res;
     }
 };
