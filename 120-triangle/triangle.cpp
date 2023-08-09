b9cba151dc5c17e0f337a5e6e1d06de6d64248ef
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-09 22:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (47.28%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int rowLen;
-    int solveMem(vector<vector<int>>&triangle,int row,int idx,vector<vector<int>> &dp){
+    vector<vector<int>> dp;
+    int solveMem(vector<vector<int>>&triangle,int row,int idx){
         if(row==rowLen){
             return 0;
         }
         if(dp[row][idx]!=-1)
             return dp[row][idx];
-        int curr=solveMem(triangle,row+1,idx,dp);
-        int next=solveMem(triangle,row+1,idx+1,dp);
+        int curr=solveMem(triangle,row+1,idx);
+        int next=solveMem(triangle,row+1,idx+1);
         return dp[row][idx]=min(curr,next)+triangle[row][idx];
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         rowLen=triangle.size();
-        vector<vector<int>> dp(rowLen,vector<int>(triangle[rowLen-1].size(),-1));
-        return solveMem(triangle,0,0,dp);
+    dp.assign(rowLen,vector<int>(triangle[rowLen-1].size(),-1));
+        return solveMem(triangle,0,0);
     }
 };
