f5f9eb7b93582dca6f6cfb0784a456e4604d8656
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-14 10:50:01 +0530

    Time: 175 ms (94.03%) | Memory: 92.1 MB (34.41%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
@@ -1,33 +1,31 @@
 #define ll long long
 class compare{
     public:
-    bool operator()(vector<int> &a,vector<int> &b){
-        return a[1]>b[1];
+    bool operator()(pair<int,int> &a,pair<int,int> &b){
+        return a.second>b.second;
     }
 }cmp;
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
         int len=nums1.size();
-        vector<vector<int>> vec(len,vector<int>(2));
+        vector<pair<int,int>> vec;
         for(int i=0;i<len;i++){
-            vec[i][0]=nums1[i];
-            vec[i][1]=nums2[i];
+            vec.push_back({nums1[i],nums2[i]});
         }
         sort(vec.begin(),vec.end(),cmp);
         priority_queue<int,vector<int>,greater<int>> minHeap;
         ll sum=0;
         for(int i=0;i<k;i++){
-            sum+=vec[i][0];
-            minHeap.push(vec[i][0]);
+            sum+=vec[i].first;
+            minHeap.push(vec[i].first);
         }
-        ll pro=sum*vec[k-1][1];
+        ll pro=sum*vec[k-1].second;
         for(int i=k;i<len;i++){
-            sum-=minHeap.top();
+            sum+=(vec[i].first-minHeap.top());
             minHeap.pop();
-            sum+=vec[i][0];
-            minHeap.push(vec[i][0]);
-            pro=max(pro,sum*vec[i][1]);
+            minHeap.push(vec[i].first);
+            pro=max(pro,sum*vec[i].second);
         }
         return pro;
     }
