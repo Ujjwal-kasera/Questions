e561b1f4fe4859d64f52bac3542172385bcbe571
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-20 13:28:17 +0530

    Time: 10 ms (33.37%) | Memory: 8.1 MB (31.29%) - LeetSync

--- 649-dota2-senate/dota2-senate.cpp
+++ 649-dota2-senate/dota2-senate.cpp
@@ -1,42 +1,71 @@
+// Approach here is to ban the closest opponent senator
+// It will not only decrease its count but also preserve 
+// our own senator from getting ban.
 class Solution {
-    bool ban(string &senate,char ch,int idx){
-        bool loop=0;
-        idx%=senate.size();
-        while(true){
-            if(idx==0)
-                loop=1;
-            if(senate[idx]==ch){
-                senate.erase(senate.begin()+idx);
-                break;
-            }
-            idx=(idx+1)%senate.size();
-        }
-        return loop;
-    }
 public:
+    // Using two Queues
     string predictPartyVictory(string senate) {
-        int dCount=0;
-        int rCount=0;
-        for(auto s:senate){
-            if(s=='R')
-                rCount++;
+        queue<int> R;
+        queue<int> D;
+        for(int i=0;i<senate.size();++i){
+            if(senate[i]=='R')
+                R.push(i);
             else
-                dCount++;
+                D.push(i);
         }
-        int idx=0;
-        while(rCount>0 && dCount>0){
-            if(senate[idx]=='R'){
-                --dCount;
-                if(ban(senate,'D',idx+1))
-                    --idx;
-            }
-            else if(senate[idx]=='D'){
-                --rCount;
-                if(ban(senate,'R',idx+1))
-                    --idx;
-            }
-            idx=(idx+1)%senate.size();
+        int rIdx,dIdx,len=senate.size();
+        while(!R.empty() && !D.empty()){
+            rIdx=R.front();
+            dIdx=D.front();
+            R.pop();
+            D.pop();
+            if(rIdx<dIdx)
+                R.push(rIdx+len);
+            else
+                D.push(dIdx+len);
         }
-        return dCount>0?"Dire":"Radiant";
+        return D.empty()?"Radiant":"Dire";
     }
+    
+    // Greedy Apporach
+//     bool ban(string &senate,char ch,int idx){
+//         bool loop=0;
+//         idx%=senate.size();
+//         while(true){
+//             if(idx==0)
+//                 loop=1;
+//             if(senate[idx]==ch){
+//                 senate.erase(senate.begin()+idx);
+//                 break;
+//             }
+//             idx=(idx+1)%senate.size();
+//         }
+//         return loop;
+//     }
+// public:
+//     string predictPartyVictory(string senate) {
+//         int dCount=0;
+//         int rCount=0;
+//         for(auto s:senate){
+//             if(s=='R')
+//                 rCount++;
+//             else
+//                 dCount++;
+//         }
+//         int idx=0;
+//         while(rCount>0 && dCount>0){
+//             if(senate[idx]=='R'){
+//                 --dCount;
+//                 if(ban(senate,'D',idx+1))
+//                     --idx;
+//             }
+//             else if(senate[idx]=='D'){
+//                 --rCount;
+//                 if(ban(senate,'R',idx+1))
+//                     --idx;
+//             }
+//             idx=(idx+1)%senate.size();
+//         }
+//         return dCount>0?"Dire":"Radiant";
+//     }
 };
