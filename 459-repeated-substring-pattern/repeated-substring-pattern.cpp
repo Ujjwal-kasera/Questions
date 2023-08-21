24e48e71b569b02202eb85fc0f99aee25ccd1c9b
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-21 17:07:17 +0530

    Time: 19 ms (64.17%) | Memory: 67.5 MB (26.02%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int len=s.size();
-        if(len==1)
-            return 0;
-        string temp=s;
-        len>>=1;
-        for(int i=0;i<len;i++){
-            temp.push_back(temp[0]);
-            temp=temp.substr(1);
-            if(s==temp)
-                return 1;
+        for(int i=1;i<=len>>1;++i){
+            if(len%i==0){
+                if(s.substr(0,len-i)==s.substr(i)){
+                    return true;
+                }
+            }
         }
-        return 0;
+        return false;        
     }
+    // bool repeatedSubstringPattern(string s) {
+    //     int len=s.size();
+    //     if(len==1)
+    //         return 0;
+    //     string temp=s;
+    //     len>>=1;
+    //     for(int i=0;i<len;i++){
+    //         temp.push_back(temp[0]);
+    //         temp=temp.substr(1);
+    //         if(s==temp)
+    //             return 1;
+    //     }
+    //     return 0;
+    // }
 };
