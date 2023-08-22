e66297abafcc4012f5b871232e3a39f8154c1002
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-22 20:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (80.09%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-        vector<char> mut = {'A','C','G','T'};
-        unordered_set<string> dict(bank.begin(),bank.end());
-        if(dict.find(end)==dict.end()) 
-            return -1;
-        int ans = 0;
-        queue<string> q;
-        q.push(start);
-        
-        while(!q.empty()) {
-            int n = q.size();
-            for(int i=0;i<n;i++) {
-                string curr = q.front();
-                q.pop();
-                if(curr==end) 
-                    return ans;
-                dict.erase(curr);
-                for(int j=0;j<8;j++) {
-                    char c = curr[j];
-                    for(int k=0;k<4;k++) {
-                        curr[j] = mut[k];
-                        if(dict.find(curr)!=dict.end())
-                            q.push(curr);
-                    }
-                    curr[j] = c;
+        unordered_set<string> st{bank.begin(),bank.end()};
+        if(!st.count(end)) return -1;
+        queue<string> Q;
+        Q.push(start);
+        int steps=0,s;
+        string cur,t;
+        while(!Q.empty()){
+            s=Q.size();
+            while(s--){
+                cur=Q.front();
+                Q.pop();
+                if(cur==end) return steps;
+                st.erase(cur);
+                for(int i=0;i<8;i++){
+                    t=cur;
+                    t[i]='A';
+                    if(st.count(t)) Q.push(t);
+                    t[i]='C';
+                    if(st.count(t)) Q.push(t);
+                    t[i]='G';
+                    if(st.count(t)) Q.push(t);
+                    t[i]='T';
+                    if(st.count(t)) Q.push(t);
                 }
             }
-            ans++;
+            steps++;
         }
-        
-        return -1;
-    }
+	return -1;
+}
 };
