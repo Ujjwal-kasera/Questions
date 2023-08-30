475a80020e13651696f6daf8d14da443d2c3c413
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-08-30 23:34:04 +0530

    Time: 89 ms (80.18%) | Memory: 54.7 MB (42.04%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     long long minimumReplacement(vector<int>& nums) {
+        int len=nums.size();
         long long ans=0,k;
-        for(int i=nums.size()-2;i>=0;--i){
+        for(int i=len-2;i>=0;--i){
             k=(nums[i]-1)/nums[i+1];
             ans+=k;
             nums[i]/=(k+1);
