6807eca2e5b55edfbe907cc3da8259f99c601179
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-09-09 12:35:50 +0530

    Time: 1231 ms (33.83%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,8 +1,13 @@
+# # Write your MySQL query statement below
+# select name 
+# from customer 
+# where id not in (
+#   select id 
+#   from customer 
+#   where referee_id=2
+#   );
+
 # Write your MySQL query statement below
 select name 
 from customer 
-where id not in (
-  select id 
-  from customer 
-  where referee_id=2
-  );
+where (referee_id is null or referee_id!=2);
