b0392643892b5020949dff9c0d6aefa464606f9c
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-09-10 16:53:15 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (43.33%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -3,11 +3,14 @@ For 2,we have 2P&2D option and placement Px/Dx,Px/Dx,Px/Dx,Px/Dx
 For 3, we have 2P&2D+P&D ie same as for 2 but one more P&D pair
 Possible position of placment for 3 will be 
 _,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ in blank we can place P&D
-Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
-Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
-Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
-Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
-Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
+Case 1: P,_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
+Case 2: Px/Dx,P,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 4 ways to place D
+Case 3: Px/Dx,Px/Dx,P,_,Px/Dx,_,Px/Dx,_ we have 3 ways to place D
+Case 4: Px/Dx,Px/Dx,Px/Dx,P,_,Px/Dx,_ we have 2 ways to place D
+Case 5: Px/Dx,Px/Dx,Px/Dx,Px/Dx,P,_ we have 1 ways to place D
+So we only need to how we can find starting number ie 5
+(2*i-1)->(2*3-1)=5
+Total ways will bi 6*5+6*4+6*3+6*2+6*1 -> 6*(5+4+3+2+1)
 */
 #define MOD 1000000007
 class Solution {
