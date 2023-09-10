58cd728763b0340d7962cde337194a5f004d43f1
Kasera <117388755+Ujjwal-kasera@users.noreply.github.com>
2023-09-10 16:49:14 +0530

    Time: 2 ms (56.67%) | Memory: 5.9 MB (71.30%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -1,3 +1,14 @@
+/* Logic is that assume we have to calculate for 3 and we know for 2->6
+For 2,we have 2P&2D option and placement Px/Dx,Px/Dx,Px/Dx,Px/Dx
+For 3, we have 2P&2D+P&D ie same as for 2 but one more P&D pair
+Possible position of placment for 3 will be 
+_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ in blank we can place P&D
+Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
+Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
+Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
+Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
+Case 1. P_,Px/Dx,_,Px/Dx,_,Px/Dx,_,Px/Dx,_ we have 5 ways to place D
+*/
 #define MOD 1000000007
 class Solution {
 public:
