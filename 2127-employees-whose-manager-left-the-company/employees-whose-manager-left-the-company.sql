007edd796e229340c2c9b39605b845570cb13fbe
Ujjwal Kasera <117388755+ujjwalkasera01@users.noreply.github.com>
2023-09-13 19:37:10 +0530

    Time: 694 ms (79.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,6 @@
+SELECT employee_id
+FROM employees
+WHERE manager_id IN (SELECT manager_id
+  FROM employees
+  WHERE manager_id NOT IN (SELECT employee_id FROM employees)) AND salary<30000
+ORDER BY employee_id;
