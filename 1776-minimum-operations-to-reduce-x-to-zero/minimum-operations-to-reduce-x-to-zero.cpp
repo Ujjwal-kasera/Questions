abdcae0b3191628c23dcf3c86ddba3dc0ab52670
Ujjwal Kasera <117388755+ujjwalkasera01@users.noreply.github.com>
2023-09-21 21:31:33 +0530

    Time: 134 ms (45.47%) | Memory: 98.8 MB (39.49%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,18 +4,17 @@ public:
         int len=nums.size(),totalSum=0,sum=0,count=-1,tmp;
         for(int i=0;i<len;++i)
             totalSum+=nums[i];
-        deque<int> q;
-        for(int i=0;i<len;++i){
-            sum+=nums[i];
-            q.push_back(nums[i]);
-            while(!q.empty() && sum>(totalSum-x)){
-                sum-=q.front();
-                q.pop_front();
+        int s=0,e=0;
+        while(e<len){
+            sum+=nums[e];
+            while(s<=e && sum>(totalSum-x)){
+                sum-=nums[s];
+                s++;
             }
             if(sum==(totalSum-x)){
-                tmp=q.size();
-                count=max(count,tmp);
+                count=max(count,e-s+1);
             }
+            e++;
         }
         return count==-1?-1:len-count;
     }
