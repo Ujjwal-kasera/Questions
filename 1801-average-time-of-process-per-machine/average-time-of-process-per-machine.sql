682fa434fe33108de060540e705709f5acb931fa
Ujjwal Kasera <117388755+ujjwalkasera01@users.noreply.github.com>
2023-09-25 23:15:08 +0530

    Time: 460 ms (66.29%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,14 @@
-SELECT 
-machine_id,
-ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp*-1 ELSE timestamp END)*1.0/(SELECT COUNT(DISTINCT process_id)),3) AS processing_time
-FROM activity
-GROUP BY machine_id
+SELECT a.machine_id,
+ROUND(AVG(b.timestamp-a.timestamp),3) AS processing_time
+FROM activity a,activity b
+WHERE a.machine_id=b.machine_id
+AND a.process_id=b.process_id
+AND a.activity_type='start'
+AND b.activity_type='end'
+GROUP BY a.machine_id;
+
+# SELECT 
+# machine_id,
+# ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp*-1 ELSE timestamp END)*1.0/(SELECT COUNT(DISTINCT process_id)),3) AS processing_time
+# FROM activity
+# GROUP BY machine_id;
